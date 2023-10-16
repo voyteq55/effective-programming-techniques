618b006df00fe5abc06017a60b50846f60af0e5f
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-16 19:45:16 +0200

    Change naming convention to camelCase

--- list01-memory-allocation/ex1.cpp
+++ list01-memory-allocation/ex1.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
 #include "ex1.h"
 
-void v_alloc_table_fill_34(int iSize) {
-    int *pt_array;
-    pt_array = new int[iSize];
+void vAllocTableFill34(int iSize) {
+    int *pArray;
+    pArray = new int[iSize];
     
     for (int i = 0; i < iSize; i++) {
-        pt_array[i] = CONST_34;
+        pArray[i] = CONST_34;
     }
 
     std::cout << "[";
     for (int i = 0; i < iSize; i++) {
         if (i + 1 != iSize) {
-            std::cout << pt_array[i] << ", ";
+            std::cout << pArray[i] << ", ";
         } else {
-            std::cout << pt_array[i] << "]" << std::endl;	
+            std::cout << pArray[i] << "]" << std::endl;	
         }
     }
 
-    delete[] pt_array;
+    delete[] pArray;
 }
 
-bool b_alloc_table_2_dim(int ***pi_table, int i_size_x, int i_size_y) {
-    if (i_size_x <=0 || i_size_y <= 0) {
+bool bAllocTable2Dim(int ***piTable, int iSizeX, int iSizeY) {
+    if (iSizeX <=0 || iSizeY <= 0) {
         return false;
     }
     
-    *pi_table = new int*[i_size_x];
-    for (int i = 0; i < i_size_x; i++) {
-        *pi_table[i] = new int[i_size_y];
+    *piTable = new int*[iSizeX];
+    for (int i = 0; i < iSizeX; i++) {
+        *piTable[i] = new int[iSizeY];
     }
 
     return true;
 }
 
 int main() {
-    v_alloc_table_fill_34(10);
+    vAllocTableFill34(10);
 }
--- list01-memory-allocation/ex1.h
+++ list01-memory-allocation/ex1.h
@@ -1,2 +1,2 @@
 const int CONST_34 = 34; 
-void v_alloc_table_fill_34(int iSize);
+void vAllocTableFill34(int iSize);
