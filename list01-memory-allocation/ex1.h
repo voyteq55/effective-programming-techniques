e0d9a0083b8b3953fa15dd4fb91945359f4f5d2b
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-16 21:52:00 +0200

    Update header file

--- list01-memory-allocation/ex1.h
+++ list01-memory-allocation/ex1.h
@@ -1,2 +1,4 @@
 const int CONST_34 = 34; 
 void vAllocTableFill34(int iSize);
+bool bAllocTable2Dim(int ***piTable, int iSizeX, int iSizeY);
+bool bDeallocTable2Dim(int **piTable, int iSizeX, int iSizeY);
