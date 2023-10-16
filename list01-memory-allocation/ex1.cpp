391f3ef07ea91713a907b02098e249dd93304d61
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-16 19:42:02 +0200

    Add solution to ex. 2 for list 01

--- list01-memory-allocation/ex1.cpp
+++ list01-memory-allocation/ex1.cpp
@@ -21,6 +21,19 @@ void v_alloc_table_fill_34(int iSize) {
     delete[] pt_array;
 }
 
+bool b_alloc_table_2_dim(int ***pi_table, int i_size_x, int i_size_y) {
+    if (i_size_x <=0 || i_size_y <= 0) {
+        return false;
+    }
+    
+    *pi_table = new int*[i_size_x];
+    for (int i = 0; i < i_size_x; i++) {
+        *pi_table[i] = new int[i_size_y];
+    }
+
+    return true;
+}
+
 int main() {
     v_alloc_table_fill_34(10);
 }
