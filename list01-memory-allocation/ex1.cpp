220cd92040f2da73ac30c3c027b1ca830787c346
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-16 19:34:29 +0200

    Convert tabs into spaces

--- list01-memory-allocation/ex1.cpp
+++ list01-memory-allocation/ex1.cpp
@@ -2,25 +2,25 @@
 #include "ex1.h"
 
 void v_alloc_table_fill_34(int iSize) {
-	int *pt_array;
-	pt_array = new int[iSize];
-	
-	for (int i = 0; i < iSize; i++) {
-		pt_array[i] = CONST_34;
-	}
+    int *pt_array;
+    pt_array = new int[iSize];
+    
+    for (int i = 0; i < iSize; i++) {
+        pt_array[i] = CONST_34;
+    }
 
-	std::cout << "[";
-	for (int i = 0; i < iSize; i++) {
-		if (i + 1 != iSize) {
-			std::cout << pt_array[i] << ", ";
-		} else {
-			std::cout << pt_array[i] << "]" << std::endl;	
-		}
-	}
+    std::cout << "[";
+    for (int i = 0; i < iSize; i++) {
+        if (i + 1 != iSize) {
+            std::cout << pt_array[i] << ", ";
+        } else {
+            std::cout << pt_array[i] << "]" << std::endl;	
+        }
+    }
 
-	delete[] pt_array;
+    delete[] pt_array;
 }
 
 int main() {
-	v_alloc_table_fill_34(10);
+    v_alloc_table_fill_34(10);
 }
